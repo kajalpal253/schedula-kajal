fe5e13aa68525408c33ad01f6904a780397be6ec
kajal <kpal53249@gmail.com>
2026-06-06 00:02:12 +0530

    Implemented role based authentication

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { PrismaService } from './auth/prisma/prisma.service';
+import { ProtectedModule } from './protected/protected.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AuthModule,ProtectedModule],
+  providers: [PrismaService],
 })
 export class AppModule {}
